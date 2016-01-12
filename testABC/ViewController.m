2e695668b594691598610f5def1068dd2602c6b3
Admin <admin@Venkis-Mac-mini.local>
2016-01-12 23:03:45 +0530

    If this works, it will be good

--- testABC/ViewController.m
+++ testABC/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.statusLabel.text = [[NSBundle mainBundle] infoDictionary][@"kHiteshURL"];
+    NSLog(@"@%", [[NSBundle mainBundle] infoDictionary][@"kHiteshURL"]);
 }
 
 - (void)didReceiveMemoryWarning {
